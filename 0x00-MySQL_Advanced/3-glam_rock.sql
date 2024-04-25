6ca9196589662ad3ba9a9810fbfbd1ae54b425a2
AzorAha1 <wizfaiz@icloud.com>
2024-04-25 22:22:00 +0100

    update

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,4 +1,3 @@
 --- band with glam rock
-SELECT band_name, (2022 - formed) as lifespan
-from metal_bands
-ORDER BY lifespan DESC
+SELECT band_name, COALESCE(split, 2922) - formed as lifespan FROM metal_bands
+WHERE style LIKE "%Glam rock%" ORDER BY lifespan DESC
