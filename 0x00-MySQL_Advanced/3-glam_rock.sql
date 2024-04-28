450ec34e2c94334fa4e88958330a96f6e0cebc8b
AzorAha1 <wizfaiz@icloud.com>
2024-04-28 18:53:03 +0100

    fixing comment

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,3 +1,3 @@
---- band with glam rock
-SELECT band_name, COALESCE(split, 2020) - formed as lifespan FROM metal_bands
+-- band with glam rock
+SELECT band_name, COALESCE(split, 2022) - formed as lifespan FROM metal_bands
 WHERE style LIKE '%Glam rock%' ORDER BY lifespan DESC;
