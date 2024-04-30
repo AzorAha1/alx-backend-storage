a4c585d7a4c1f632420af93890e4a43ec470dff8
AzorAha1 <wizfaiz@icloud.com>
2024-04-30 12:20:31 +0100

    8-all.py

---
+++ 0x01-NoSQL/8-all.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""using mongodb"""
+
+
+def list_all(mongo_collection):
+    """using python to list all documents in a collection
+
+    Args:
+        mongo_collection (list): list
+
+    Returns:
+        list: list of documents
+    """
+    documents = []
+    if (mongo_collection.count_documents({}) == 0):
+        return document
+    for document in mongo_collection.find():
+        documents.append(document)
+    return documents
