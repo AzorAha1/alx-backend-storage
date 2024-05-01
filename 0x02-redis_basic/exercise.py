dc5adcf6952ad284501c67cc9a19fcdef7b82c48
AzorAha1 <wizfaiz@icloud.com>
2024-05-02 00:29:14 +0100

    exercise.py

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -3,6 +3,7 @@
 import redis
 import uuid
 from typing import Union
+from typing import Callable
 
 
 class Cache:
@@ -12,7 +13,7 @@ class Cache:
         self._redis = redis.Redis()
         self._redis.flushdb()
 
-    def store(self, data:Union[str, bytes, int, float]) -> str:
+    def store(self, data: Union[str, bytes, int, float]) -> str:
         """_summary_
         Args:
             data (any): _description_
@@ -23,3 +24,40 @@ class Cache:
         randomkey = str(uuid.uuid4())
         self._redis.set(name=randomkey, value=data)
         return randomkey
+
+    def get(self, key: str, fn: Callable[[str], str]) -> str:
+        """_summary_
+
+        Args:
+            key (str): _description_
+            fn (Callable[[str], str]): _description_
+
+        Returns:
+            str: _description_
+        """
+        if not key not in self._redis:
+            return None
+        data = self._redis.get(key)
+        return fn(data)
+
+    def get_str(self, key: str) -> str:
+        """_summary_
+
+        Args:
+            key (str): _description_
+
+        Returns:
+            str: _description_
+        """
+        return self.get(key=key, fn=str)
+
+    def get_int(self, key: int) -> int:
+        """_summary_
+
+        Args:
+            key (int): _description_
+
+        Returns:
+            int: _description_
+        """
+        return self.get(key=key, fn=int)
