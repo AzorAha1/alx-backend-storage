9f7c60fe6c71c573d441327a8fbbb02b6bee8535
AzorAha1 <wizfaiz@icloud.com>
2024-05-02 00:39:58 +0100

    exercise.py

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -52,7 +52,7 @@ class Cache:
         value = self._redis.get(key)
         return self.get(key=key, fn=value.decode('utf-8'))
 
-    def get_int(self, key: int) -> int:
+    def get_int(self, key: str) -> int:
         """_summary_
 
         Args:
@@ -62,3 +62,15 @@ class Cache:
             int: _description_
         """
         return self.get(key, int)
+
+cache = Cache()
+
+TEST_CASES = {
+    b"foo": None,
+    123: int,
+    "bar": lambda d: d.decode("utf-8")
+}
+
+for value, fn in TEST_CASES.items():
+    key = cache.store(value)
+    assert cache.get(key, fn=fn) == value
